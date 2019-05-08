bad31676b8eab3f4744d426b2c6bb1a623059f7a
Pontus <lmartins@pontusnetworks.com>
2019-05-08 08:27:18 +0100

    flow.xml.gz file with controller services enabled + replacement for extract text

--- build-local.sh
+++ build-local.sh
@@ -3,14 +3,15 @@
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $DIR/base
 docker build --rm . -t pontusvisiongdpr/pontus-extract-nifi-base 
-docker push pontusvisiongdpr/pontus-extract-nifi-base
 
 cd $DIR/full-nifi
 docker build --rm . -t pontusvisiongdpr/pontus-extract-nifi
-docker push pontusvisiongdpr/pontus-extract-nifi
 
 cd $DIR/full-minifi
 docker build --rm . -t pontusvisiongdpr/pontus-extract-minifi
+
 docker push pontusvisiongdpr/pontus-extract-minifi
+docker push pontusvisiongdpr/pontus-extract-nifi-base
+docker push pontusvisiongdpr/pontus-extract-nifi
 
 
