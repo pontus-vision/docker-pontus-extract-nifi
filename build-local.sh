7266caa4926eed967fcd12e32ce5aa39bdb7a8e7
dev <dev@pontusvision.com>
2019-07-01 02:02:46 +0100

    added new flow file for gdpr stable version

--- build-local.sh
+++ build-local.sh
@@ -11,8 +11,8 @@ docker build --rm . -t pontusvisiongdpr/pontus-extract-nifi
 cd $DIR/full-minifi
 docker build --rm . -t pontusvisiongdpr/pontus-extract-minifi
 
-docker push pontusvisiongdpr/pontus-extract-minifi
 docker push pontusvisiongdpr/pontus-extract-nifi-base
 docker push pontusvisiongdpr/pontus-extract-nifi
+docker push pontusvisiongdpr/pontus-extract-minifi
 
 
