ec2f74abbb747b7096b2b6cf7bfe38940a310ad0
Leo Martins <lmartins@pontusvision.com>
2021-01-01 14:23:37 +0000

    added updated flowfile

--- build-docker.sh
+++ build-docker.sh
@@ -1,18 +1,28 @@
-#!/bin/bash
+#!/bin/bash -x
+
+export DOCKER_BUILDKIT=0
+export COMPOSE_DOCKER_CLI_BUILD=0
+
 
 set -e
+NIFI_TAG=${NIFI_TAG:-1.12.1}
 DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 cd $DIR/base
-docker build --rm . -t pontusvisiongdpr/pontus-extract-nifi-base 
+docker build --build-arg NIFI_TAG=${NIFI_TAG} --rm . -t pontusvisiongdpr/pontus-extract-nifi-base:${NIFI_TAG}
 
 cd $DIR/full-nifi
-docker build --rm . -t pontusvisiongdpr/pontus-extract-nifi
+docker build --build-arg NIFI_TAG=${NIFI_TAG} --rm . -t pontusvisiongdpr/pontus-extract-nifi:${NIFI_TAG}
+
+cd $DIR/mysql-demo
+docker build --rm . -t pontusvisiongdpr/pontus-extract-nifi-mysql-demo
+
 
 #cd $DIR/full-minifi
 #docker build --rm . -t pontusvisiongdpr/pontus-extract-minifi
 
-docker push pontusvisiongdpr/pontus-extract-nifi-base
-docker push pontusvisiongdpr/pontus-extract-nifi
+docker push pontusvisiongdpr/pontus-extract-nifi-base:${NIFI_TAG}
+docker push pontusvisiongdpr/pontus-extract-nifi:${NIFI_TAG}
+docker push pontusvisiongdpr/pontus-extract-nifi-mysql-demo
 #docker push pontusvisiongdpr/pontus-extract-minifi
 
 
